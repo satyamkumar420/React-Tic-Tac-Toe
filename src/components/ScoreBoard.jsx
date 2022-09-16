bd964b86b88307852a2b6c0f91944dc602aaad8e
Satyam Kumar <98641231+satyamkumar420@users.noreply.github.com>
2022-09-16 13:05:21 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ScoreBoard.css";
+const ScoreBoard = ({ scores, xPlaying }) => {
+  const { xScore, oScore } = scores;
+  return (
+    <>
+      <div className="scoreBoard">
+        <span className={`score x-score ${!xPlaying && "inactive"}`}>
+          X - {xScore}
+        </span>
+        |
+        <span className={`score o-score ${xPlaying && "inactive"}`}>
+          O - {oScore}
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default ScoreBoard;
